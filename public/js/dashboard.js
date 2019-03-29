ff9ca740169b56c9ed002946cee992ce5d20509a
rasoul.makhi <rasool.makhigmail.com>
2019-03-29 12:13:41 +0430

    Jalase 19

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,25 +4,37 @@ $(document).ready(function () {
     $('.pdp').persianDatepicker();
     $('[data-toggle=popover]').popover();
 
+    // clone transaction rows
+    $('#add-row').click(function () {
+        $('.transaction-row').clone().appendTo('#transaction-rows');
+    });
+
     //transactions
-    $('.first-amount').keyup(function () {
-
-        var firstAmount = $(this).val();
-        var discountPercent = $('#new-transaction').attr('data-discount-percent');
-        var giftPercent = $('#new-transaction').attr('data-gift-percent');
-
-        var discountAmount = (firstAmount*discountPercent) / 100;
-        var payableAmount = firstAmount - discountAmount;
-        var giftAmount = (payableAmount*giftPercent) / 100;
-
-        $('.final-discount').html(addCommas(discountAmount));
-        $('.final-payable').html(addCommas(payableAmount));
-        $('.final-gift').html(addCommas(giftAmount));
-
+    $('.first-amount, .cash-discount, .count').keyup(function () {
+        updateRow();
     });
 
 
 });
+
+function updateRow() {
+    var discountPercent = $('#new-transaction').attr('data-discount-percent');
+    var giftPercent = $('#new-transaction').attr('data-gift-percent');
+
+    var firstAmount = $('.first-amount').val();
+    var count = $('.count').val() ? $('.count').val() : 0;
+    var cashDiscountAmount = $('.cash-discount').val() ? $('.cash-discount').val() : 0;
+
+    var clubDiscountAmount = (firstAmount*discountPercent) / 100;
+    var payableAmount = firstAmount - clubDiscountAmount - cashDiscountAmount;
+    var giftAmount = (payableAmount*giftPercent) / 100;
+
+    $('.final-club-discount').html(addCommas(count * clubDiscountAmount));
+    $('.final-cash-discount').html(addCommas(count * cashDiscountAmount));
+    $('.final-count').html(addCommas(count));
+    $('.final-payable').html(addCommas(count * payableAmount));
+    $('.final-gift').html(addCommas(count * giftAmount));
+}
 
 function addCommas(nStr) {
     nStr += '';
--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -3,49 +3,98 @@
     <title> تراکنش جدید </title>
 @endsection
 @section('content')
+
+    <div class="text-center mb-3">
+        <button type="button" class="btn btn-outline-success" id="add-row">
+            <i class="fa fa-plus ml-1"></i> مورد جدید
+        </button>
+    </div>
+
     <div class="card card-body">
-        <form class="row justify-content-center" action="{{url("transactions")}}" method="post" id="new-transaction"
+        <form action="{{url("transactions")}}" method="post" id="new-transaction"
             data-gift-percent="10" data-discount-percent="5">
             @csrf
 
-            <div class="form-group col-md-3">
-                <label for="customer"> انتخاب مشتری </label>
-                <select class="selectpicker mt-2" name="customer_id" id="customer" data-live-search="true" title="جستجوی مشتری">
-                    @foreach ($customers as $customer)
-                        <option value="{{$customer->id}}" @if(old('customer_id') == $customer->id) selected @endif>
-                            {{$customer->name()}} - {{$customer->mobile()}}
-                        </option>
-                    @endforeach
-                </select>
+            {{-- <div class="row">
+                <div class="form-group col-md-3 mx-auto">
+                    <label for="customer"> انتخاب مشتری </label>
+                    <select class="selectpicker mt-2" name="customer_id" id="customer" data-live-search="true" title="جستجوی مشتری">
+                        @foreach ($customers as $customer)
+                            <option value="{{$customer->id}}" @if(old('customer_id') == $customer->id) selected @endif>
+                                {{$customer->name()}} - {{$customer->mobile()}}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+            </div> --}}
+
+            <div id="transaction-rows">
+                <div class="row transaction-row">
+
+                    <div class="col-md-5">
+
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <label for="service"> نام محصول یا خدمات </label>
+                                <input type="text" name="service" value="{{old('service')}}" id="service" class="form-control mt-2">
+                            </div>
+
+                            <div class="form-group col-md-6">
+                                <label for="amount"> قیمت اولیه (به تومان)</label>
+                                <input type="number" name="amount" value="{{old('amount')}}" id="amount" class="form-control first-amount mt-2">
+                            </div>
+
+                            <div class="form-group col-md-6">
+                                <label for="cash-discount"> تخفیف نقدی </label>
+                                <input type="number" name="cash_discount" value="{{old('cash_discount') ?? 0}}" id="cash-discount" class="form-control cash-discount mt-2">
+                            </div>
+
+                            <div class="form-group col-md-6">
+                                <label for="count"> تعداد </label>
+                                <input type="number" name="count" value="{{old('count') ?? 1}}" id="count" class="form-control count mt-2">
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-7">
+                        <div class="row">
+                            <div class="col-md-6 p-1">
+                                <ul class="list-group p-0">
+                                    <li class="list-group-item list-group-item-info">
+                                        تخفیف :
+                                        <span class="final-club-discount" title="تخفیف باشگاه مشتریان" data-toggle="tooltip"> 0 </span>
+                                        <i class="fa fa-plus text-primary"></i>
+                                        <span class="final-cash-discount" title="تخفیف نقدی" data-toggle="tooltip"> 0 </span>
+                                        تومان
+
+                                    </li>
+                                    <li class="list-group-item list-group-item-info">
+                                        تعداد : <span class="final-count"> 1 </span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6 p-1">
+                                <ul class="list-group p-0">
+                                    <li class="list-group-item list-group-item-info">
+                                        قابل پرداخت : <span class="final-payable"> 0 </span> تومان
+                                    </li>
+                                    <li class="list-group-item list-group-item-info">
+                                        اعتبار هدیه : <span class="final-gift"> 0 </span> تومان
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr class="w-100">
+                </div>
             </div>
 
-            <div class="form-group col-md-3">
-                <label for="service"> نام محصول یا خدمات </label>
-                <input type="text" name="service" value="{{old('service')}}" id="service" class="form-control mt-2">
-            </div>
-
-            <div class="form-group col-md-3">
-                <label for="amount"> قیمت اولیه (به تومان)</label>
-                <input type="number" name="amount" value="{{old('amount')}}" id="amount" class="form-control first-amount mt-2">
-            </div>
-
-            <div class="col-md-3">
-                <ul class="list-group p-0">
-                    <li class="list-group-item list-group-item-info">
-                        تخفیف : <span class="final-discount"> 0 </span> تومان
-                    </li>
-                    <li class="list-group-item list-group-item-info">
-                        قابل پرداخت : <span class="final-payable"> 0 </span> تومان
-                    </li>
-                    <li class="list-group-item list-group-item-info">
-                        اعتبار هدیه : <span class="final-gift"> 0 </span> تومان
-                    </li>
-                </ul>
-            </div>
-
-            <hr class="w-100">
-            <div class="col-md-2">
-                <button type="submit" class="btn btn-primary btn-block"> ثبت تراکنش </button>
+            <div class="row">
+                <div class="col-md-2 mx-auto">
+                    <button type="submit" class="btn btn-primary btn-block"> ثبت تراکنش </button>
+                </div>
             </div>
 
         </form>
